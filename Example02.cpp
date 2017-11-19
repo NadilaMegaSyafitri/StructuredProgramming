64bbb5b6ddefe10fdaed48b5fc811e2e7a0491fa
NadilaMegaSyafitri <32352445+NadilaMegaSyafitri@users.noreply.github.com>
2017-11-19 19:32:51 +0700

    Example 02

---
+++ Example02.cpp
@@ -0,0 +1,16 @@
+// Tugas: Mengkonversi kaki / inci ke meter
+// Input: panjang di kaki dan inci, dengan kemungkinan desimal
+// bagian inci
+// Output: panjang dalam meter, dengan 2 tempat desimal, yang mana
+// adalah 'sentimeter' yang ditentukan dalam masalah.
+
+#include <iostream>
+using namespace std;
+void input(int& kaki, double& inci);
+void convert (int kai, double inci, double& meter);
+void output(int kai, double inci, double meter);
+
+int main()
+{
+
+}
