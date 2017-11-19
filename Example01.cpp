afbeccccd89b05e1721d063340aa9f80c2b8c862
NadilaMegaSyafitri <32352445+NadilaMegaSyafitri@users.noreply.github.com>
2017-11-19 19:10:11 +0700

    Example 01

---
+++ Example01.cpp
@@ -0,0 +1,14 @@
+// Tugas: Mengkonversi notulen 24 jam ke notasi AM / AM 12 jam.
+// Masukan: 24 jam
+// Output: waktu 12 jam yang sesuai, dengan indikasi AM / PM
+// Diperlukan: 3 fungsi: input, konversi, dan output.
+
+#include <iostream>
+using namespace std;
+void input(int& jam24, int& menit);
+void convert(int& jam, char& AMPM);
+void output(int jam, int menit, char AMPM);
+int main()
+{
+	
+}
